2f28ed5ee503b5b38c0cba75c7335d00ec8ff493
carlosjunod <carlos@junod.cl>
2017-08-08 21:32:35 -0400

    saving in localstorage

--- src/index.js
+++ src/index.js
@@ -8,8 +8,16 @@ import { createStore } from 'redux'
 
 import medicineReducer from './core-modules/reducers'
 
-let store = createStore(medicineReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// Middleware to save in localStorage
+import { loadState, saveState } from './localStorage'
+const persistedState = loadState()
 
+const store = createStore(medicineReducer, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+// saving in local storage
+store.subscribe( () => {
+  saveState(store.getState())
+} )
 
 ReactDOM.render(
   <Provider store={store} >
